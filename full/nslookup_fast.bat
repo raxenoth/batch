dd7437b4fe57e74af6650a458df80e5b7c60a2d5
raxenoth <raxenoth@users.noreply.github.com>
2014-01-17 05:01:25 -0500

    Update nslookup_fast.bat

--- full/nslookup_fast.bat
+++ full/nslookup_fast.bat
@@ -1,11 +1,12 @@
 @echo off
+ipconfig /flushdns
 echo.
 echo =======================
 echo =NS Lookup For WebPass=
 echo =======================
 echo.
 echo Parsing Ip List for IIS
-PING 1.1.1.1 -n 10 -w 500 >NUL
+PING 1.1.1.1 -n 10 -w 350 >NUL
 echo Now scanning IIS servers
 echo Please wait
 for /f %%a in (IIS_IP.txt) do (nslookup %%a  2> nul|find /i "%%a">nul||(echo %%a)) >>.\IIS_fast_failed.txt
@@ -13,11 +14,14 @@ echo IIS Complete!
 echo.
 echo.
 echo Parsing Ip List for SQL
-PING 1.1.1.1 -n 10 -w 500 >NUL
+PING 1.1.1.1 -n 10 -w 350 >NUL
 echo Now scanning SQL servers
 echo Please wait
 @echo off
 for /f %%a in (SQL_IP.txt) do (nslookup %%a  2> nul|find /i "%%a">nul||(echo %%a)) >>.\SQL_fast_failed.txt
 echo SQL Complete!
 echo.
+@echo off
+ipconfig /flushdns
 echo.
+
