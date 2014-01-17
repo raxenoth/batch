8da81a9c483ab521ba98e5dfb07461c191208da1
raxenoth <raxenoth@users.noreply.github.com>
2014-01-17 05:01:50 -0500

    Update nslookup_slow.bat

--- full/nslookup_slow.bat
+++ full/nslookup_slow.bat
@@ -11,23 +11,23 @@ echo.
 echo This tool is slow, and bandwith intensive.
 echo Use CTRL + C to return to the main menu, or any other key to continue.
 pause
-IF EXIST "IIS_fast_failed.txt" (
+IF EXIST "IIS_bad_dns.txt" (
 echo.
 echo IIS non-responsive server list found!
 echo IIS list loaded.
 echo.
 echo Probing IIS Servers.
-for /f %%a in (IIS_fast_failed.txt) do (nbtstat -A %%a > nul|find /i "%%a">nul||(echo %%a)) >>.\failed_hosts.txt
+for /f %%a in (IIS_bad_dns.txt) do (nbtstat -A %%a > nul|find /i "%%a">nul||(echo %%a)) >>.\failed_hosts.txt
 echo.
 echo IIS list complete.
 )
-IF EXIST "SQL_fast_failed.txt" (
+IF EXIST "SQL_bad_dns.txt" (
 echo.
 echo SQL non-responsive server list found!
 echo SQL list loaded.
 echo.
 echo Probing SQL Servers.
-for /f %%a in (SQL_fast_failed.txt) do (nbtstat -A %%a > nul|find /i "%%a">nul||(echo %%a)) >>.\failed_hosts.txt
+for /f %%a in (SQL_bad_dns.txt) do (nbtstat -A %%a > nul|find /i "%%a">nul||(echo %%a)) >>.\failed_hosts.txt
 )
 
 echo Complete
